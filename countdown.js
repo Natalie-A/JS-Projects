3c61408921825d58824e87b13942c2bb316627da
Natalie Aoya <aoyanatalie40@gmail.com>
2023-09-05 08:46:45 +0300

    countdown timer added

---
+++ countdown.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    var birthday = new Date("Mar 5, 2024 00:00:00").getTime();
+
+    var x = setInterval(function() {
+
+        var now = new Date().getTime();
+
+        var distance = birthday - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("time").innerHTML = days + "d " + hours + "h " +
+            minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("time").innerHTML = "EXPIRED";
+        }
+
+    }, 1000);
+}
